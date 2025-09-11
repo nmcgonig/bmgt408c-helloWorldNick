24120193b31cbf18cab2203c2a0a118f5c44df13
Nick <nmcgonig@terpmail.umd.edu>
2025-09-11 17:31:36 -0400

    Changed route of main page and Name

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():  # put application's code here
-    return 'Hello World from Robert Smith!'
+    return 'Hello World from Nicholas McGonigle!'
 
 
 if __name__ == '__main__':
